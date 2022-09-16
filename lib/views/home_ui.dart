d97e24e6805381108ee280cb9946af6462427450
s6352410009 <s635241009@sau.ac.th>
2022-09-16 12:29:36 +0700

    Code V.1 by Chanitta

---
+++ lib/views/home_ui.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class HomeUI extends StatefulWidget {
+  const HomeUI({super.key});
+
+  @override
+  State<HomeUI> createState() => _HomeUIState();
+}
+
+class _HomeUIState extends State<HomeUI>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //widget หลักของหน้าจอ
+    return Scaffold(
+      backgroundColor: Colors.green,
+    );
+  }
+}
